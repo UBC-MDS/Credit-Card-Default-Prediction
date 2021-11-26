c57f55701b8b38a4699632ab7cba5fabef9b0a98
Rohit Rawat <52863845+garhwalinauna@users.noreply.github.com>
2021-11-26 15:22:15 -0800

    Updated the python script. Works now.

--- src/model_train_tune.py
+++ src/model_train_tune.py
@@ -1,13 +1,13 @@
 # author: Rohit Rawat
 # date: 2021-11-26
 
-"""This script prints out docopt args.
-Usage: demo.py <arg1> [<arg4>]  --arg2=<arg2> [--arg3=<arg3>]
+"""This script selects the best model and hypertunes the parameters correspondinng to that model.
+Usage: model_train_tune.py --path=<path> --score_file=<score_file> [--model_path=<model_path>]
 
 Options:
 --path=<path>               Path to read file from
---score_file=<score_file>   Path (including filename) of where to locally save cross val score
---model_path=<model_path>   Path for the model pickle file [default: ../results/models/final_svc.pkl]
+--score_file=<score_file>   Path (including filename) saving the different model scores
+--model_path=<model_path>   Path for the model pickle file [default: ../results/models/final_model.pkl]
 """ 
 
 import os
@@ -15,6 +15,7 @@ import pandas as pd
 from docopt import docopt
 import numpy as np
 import sys
+import pickle
 
 from sklearn.model_selection import (
     RandomizedSearchCV,
@@ -26,7 +27,6 @@ from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.pipeline import Pipeline, make_pipeline
@@ -36,8 +36,6 @@ from sklearn.preprocessing import (
 )
 from sklearn.svm import SVC
 from scipy.stats import loguniform
-
-import pickle
 
 
 opt = docopt(__doc__)
@@ -74,7 +72,6 @@ def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
 
     for i in range(len(mean_scores)):
         out_col.append(mean_scores[i])
-
     return pd.Series(data=out_col, index=mean_scores.index)
 
 def train_multiple_models(preprocessor, X_train, y_train, scoring_metrics):
@@ -105,7 +102,7 @@ def hyperparameter_tuning(preprocessor, X_train, y_train, scoring_metrics):
         n_jobs=-1,
         random_state=123,
         scoring=scoring_metrics,
-        refit='f-1'
+        refit='roc_auc'
     )
     lr_random.fit(X_train, y_train)
 
@@ -128,11 +125,10 @@ def main(path, out_file, model_path):
     
     # Splitting between Features
     X_train, y_train = train_df.drop(columns=["default payment next month"]), train_df["default payment next month"]
-
     #Dividing the different types of features
     categorical_features = ['SEX', 'EDUCATION', 'MARRIAGE', 'PAY_0', 'PAY_2','PAY_3', 'PAY_4', 'PAY_5', 'PAY_6']
     numeric_features = ['LIMIT_BAL', 'AGE'] + X_train.columns.tolist()[11:]
-    drop_features = ['ID']
+    #drop_features = ['ID']
 
     preprocessor = make_preprocessor(numeric_features, categorical_features)
 
@@ -153,12 +149,10 @@ def main(path, out_file, model_path):
 
     try:
         pickle.dump(best_model, open(str(model_path),"wb"))
-        pickle.dump(best_params, open(str(os.path.dirname(out_file))+"/final_parameters.pkl","wb"))
     except:
         os.makedirs(os.path.dirname(model_path))
         directory = os.path.dirname(model_path)
-        pickle.dump(best_model, open(model_path,"wb"))
-        pickle.dump(best_params, open(str(directory)+"/final_parameters.pkl","wb"))
+        pickle.dump(best_params, open(str(directory)+"/final_model.pkl","wb"))
 
     
 
